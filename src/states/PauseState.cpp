6cca4bd89d0526f0819be79be972bace9dc6dd37
Jorge Luis Rodríguez González <egroj97@gmail.com>
2023-01-28 08:42:06 -0400

    Support for pausing the game

---
+++ src/states/PauseState.cpp
@@ -0,0 +1,44 @@
+/*
+    ISPPJ1 2023
+    Study Case: Flappy Bird
+
+    Author: Jorge Rodríguez
+    egroj97@gmail.com
+
+    This file contains the definition of the class PauseState.
+*/
+
+#include <Settings.hpp>
+#include <src/text_utilities.hpp>
+#include <src/states/StateMachine.hpp>
+#include <src/states/PauseState.hpp>
+
+PauseState::PauseState(StateMachine* sm) noexcept
+    : BaseState{sm}
+{
+
+}
+
+void PauseState::enter(std::shared_ptr<World> _world, std::shared_ptr<Bird> _bird) noexcept
+{
+    world = _world;
+    bird = _bird;
+}
+
+void PauseState::handle_inputs(const sf::Event& event) noexcept
+{
+    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+    {
+        state_machine->change_state("playing", world, bird);
+    }
+}
+
+void PauseState::update(float dt) noexcept {
+
+}
+
+void PauseState::render(sf::RenderTarget& target) const noexcept
+{
+    world->render(target);
+    render_text(target, Settings::VIRTUAL_WIDTH / 2, Settings::VIRTUAL_HEIGHT / 2, "Paused", Settings::FLAPPY_TEXT_SIZE, "font", sf::Color::White, true);
+}
---
+++ src/states/PauseState.hpp
@@ -0,0 +1,34 @@
+/*
+    ISPPJ1 2023
+    Study Case: Flappy Bird
+
+    Author: Jorge Rodríguez
+    egropj97@gmail.com
+
+    This file contains the declaration of the class PauseState.
+*/
+
+#pragma once
+
+#include <src/Bird.hpp>
+#include <src/World.hpp>
+#include <src/states/BaseState.hpp>
+
+class PauseState: public BaseState
+{
+
+public:
+    PauseState(StateMachine* sm) noexcept;
+
+    void enter(std::shared_ptr<World> _world = nullptr, std::shared_ptr<Bird> _bird = nullptr) noexcept override;
+
+    void handle_inputs(const sf::Event& event) noexcept override;
+
+    void render(sf::RenderTarget& target) const noexcept override;
+
+    void update(float dt) noexcept override;
+
+private:
+    std::shared_ptr<Bird> bird;
+    std::shared_ptr<World> world;
+};
