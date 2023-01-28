cf8308d3f3d904e0aa0d5f68d12578c96b81c30b
Jorge Luis Rodríguez González <egroj97@gmail.com>
2023-01-28 10:01:48 -0400

    Skeleton for strategy pattern

---
+++ src/Gameboard.hpp
@@ -0,0 +1,10 @@
+/*
+    ISPPJ1 2023
+    Study Case: Flappy Bird
+
+    Author: Jorge Luis Rodríguez González
+    egroj97@gmail.com
+
+    This file contains the declaration of the class Game.
+*/
+
